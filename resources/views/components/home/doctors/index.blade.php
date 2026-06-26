52ed66331fe927e9b7458491c4eb408b6a7fec6e
islam <islambassiem@icloud.com>
2026-06-26 17:47:24 +0300

    changing doctors to team

--- resources/views/components/home/doctors/index.blade.php
+++ resources/views/components/home/doctors/index.blade.php
@@ -1,5 +1,5 @@
 <section class="container mx-auto scroll-mt-36" id="doctors">
-    <p class="my-10 text-primary-950 text-center text-3xl md:text-5xl font-extrabold">Meet our Doctors</p>
+    <p class="my-10 text-primary-950 text-center text-3xl md:text-5xl font-extrabold">Meet our Team</p>
     <p class="my-5 text-primary-950 text-center text-xl md:text-3xl font-semibold max-w-xl mx-auto">Dedicated & Experienced Team</p>
 
     <div class="grid grid-cols-2 xl:grid-cols-4 gap-8">
